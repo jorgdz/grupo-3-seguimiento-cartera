15e1959ae1de2052e194b1afe3f3a353900ee36b
Jorge Diaz Montoya <57382609+jorgdz@users.noreply.github.com>
2021-03-25 01:33:46 -0500

    Agregar reportes de clientes deudores y estado de cuotas. (#4)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,6 +3,25 @@
 @section('title', 'Administración - Planes de pago')
 @section('content')
 
+   <div class="content-header">
+      <div class="container-fluid">
+         <div class="row mb-2">
+            <div class="col-sm-12">
+               <ol class="breadcrumb float-sm-left">
+                  @if(Auth::user()->perfil_actualizado)
+                        @can('pagos.report-general')
+                           <li class="breadcrumb-item"><a class="btn btn-success" href="/pagos/report-general/Aldia" target="_blank">Clientes Al día</a></li>
+                           <li class="breadcrumb-item"><a class="btn btn-info" href="/pagos/report-general/Pendiente" target="_blank">Clientes Pendientes</a></li>
+                           <li class="breadcrumb-item"><a class="btn btn-danger" href="/pagos/report-general/Incumplido" target="_blank">Clientes incumplidos</a></li>
+                        @endcan
+                  @endif
+               </ol>
+            </div>
+         </div>
+      </div>
+   </div>
+   
+   <hr>
    <dashboard-component></dashboard-component>
     
 @endsection
